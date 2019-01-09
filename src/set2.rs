1a73ef1ddb5edb2df3aff9843d00d6a44f5a27fa
Dato Simó <dato@net.com.org.es>
2019-01-09 20:41:59 -0300

    decrypt_aes_128_cbc: finalize() just once, use one buffer only.

--- src/set2.rs
+++ src/set2.rs
@@ -25,45 +25,44 @@ pub fn decrypt_aes_128_cbc(data: &[u8], key: &[u8], iv: &[u8]) -> Result<Vec<u8>
   assert_eq!(0, data.len() % keylen);
 
   let cipher = Cipher::aes_128_ecb();
-  let mut ret = Vec::with_capacity(data.len());
-  let mut prev = iv.to_owned();
-  let mut buf = vec![0; keylen + cipher.block_size()];
-
-  // Technically this should be inside the loop, because API docs
-  // mandate that update() not be called after finalize().
   let mut crypt = Crypter::new(cipher, Mode::Decrypt, key, None)?;
   crypt.pad(false);
 
+  let mut n = 0;
+  let mut tot = 0;
+  let mut buf = vec![0; data.len() + cipher.block_size()];
+  let mut prev = iv.to_owned();
+
   for block in data.chunks(keylen) {
     assert_eq!(block.len(), keylen);
-    let mut n = 0;
 
-    n += crypt.update(block, &mut buf)?;
-    n += crypt.finalize(&mut buf[n..])?;
+    n += crypt.update(block, &mut buf[tot..])?;
 
     // Must check xor_zip() return value because it does nothing
     // if the slices are different in size.
-    if !crate::set1::xor_zip(&mut buf[..n], &prev) {
+    if !crate::set1::xor_zip(&mut buf[tot..n], &prev) {
       bail!("xor_zip() returned false");
     }
-
-    ret.extend(&buf[..n]);
 
     // This hopefully doesn't allocate.
     prev.truncate(0);
     prev.extend_from_slice(block); /* It's certainly more readable than:
                                     * prev.splice(.., block.iter().cloned()); */
+    tot = n;
   }
 
+  n += crypt.finalize(&mut buf[tot..])?;
+  buf.truncate(n);
+
   // Undo PKCS#7 padding.
-  if let Some(&b) = ret.last() {
-    let start = ret.len() - b as usize;
-    if ret[start..].iter().all(|&c| c == b) {
-      ret.truncate(start);
+  if let Some(&b) = buf.last() {
+    let start = buf.len() - b as usize;
+    if buf[start..].iter().all(|&c| c == b) {
+      buf.truncate(start);
     }
   }
 
-  Ok(ret)
+  Ok(buf)
 }
 
 //
