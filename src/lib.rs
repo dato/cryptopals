08b674b3bab807e80a9f97e8e843b4cbeeda2ceb
Dato Simó <dato@net.com.org.es>
2019-01-11 15:54:33 -0300

    Change assert_eq! from (expected, actual) to (actual, expected).
    This way, when Rust prints:
    
        assertion failed ‘(left == right)’:
    
          left: 7
          right: 10
    
    we can think of ‘right’ as also meaning ‘correct’: “Answer 7 is not
    right.” Isn’t this cute?

--- src/lib.rs
+++ src/lib.rs
@@ -34,8 +34,8 @@ mod test {
     // https://cryptopals.com/sets/1/challenges/1
     let hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
     assert_eq!(
-      "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t",
-      hex_to_base64(hex).unwrap()
+      hex_to_base64(hex).unwrap(),
+      "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
     );
   }
 
@@ -45,7 +45,7 @@ mod test {
     let mut a = HEX.decode(b"1c0111001f010100061a024b53535009181c").unwrap();
     let b = HEX.decode(b"686974207468652062756c6c277320657965").unwrap();
     assert!(xor_zip(&mut a, &b));
-    assert_eq!("746865206b696420646f6e277420706c6179", HEX.encode(&a));
+    assert_eq!(HEX.encode(&a), "746865206b696420646f6e277420706c6179");
   }
 
   #[test]
@@ -56,8 +56,8 @@ mod test {
       .decode(b"1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
       .unwrap();
     assert_eq!(
-      "Cooking MC's like a pound of bacon",
-      break_xor_byte(&bytes).result
+      break_xor_byte(&bytes).result,
+      "Cooking MC's like a pound of bacon"
     );
   }
 
@@ -65,7 +65,7 @@ mod test {
   fn challenge_04() {
     // Detect single-character XOR
     // https://cryptopals.com/sets/1/challenges/4
-    assert_eq!("Now that the party is jumping\n", find_xor_byte("input/04"));
+    assert_eq!(find_xor_byte("input/04"), "Now that the party is jumping\n");
   }
 
   #[test]
@@ -76,11 +76,11 @@ mod test {
       b"Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal".to_owned();
     xor_cycle(&mut bytes, b"ICE");
     assert_eq!(
+      HEX.encode(&bytes),
       concat!(
         "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272",
         "a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
-      ),
-      HEX.encode(&bytes)
+      )
     );
   }
 
@@ -88,7 +88,7 @@ mod test {
   fn challenge_06() {
     // Break repeating-key XOR
     // https://cryptopals.com/sets/1/challenges/6
-    assert_eq!(37, hamming_distance(b"this is a test", b"wokka wokka!!!"));
+    assert_eq!(hamming_distance(b"this is a test", b"wokka wokka!!!"), 37);
     assert_eq!(
       break_xor_cycle(&crate::read_base64("input/06")),
       b"Terminator X: Bring the noise"
@@ -100,10 +100,10 @@ mod test {
     // AES in ECB mode.
     // https://cryptopals.com/sets/1/challenges/7
     let res = decrypt_aes_128_ecb("input/07", b"YELLOW SUBMARINE").unwrap();
-    assert_eq!(2876, res.len());
+    assert_eq!(res.len(), 2876);
     assert_eq!(
-      "Play that funky music ",
-      String::from_utf8_lossy(&res).lines().last().unwrap()
+      String::from_utf8_lossy(&res).lines().last().unwrap(),
+      "Play that funky music "
     );
   }
 
@@ -112,8 +112,8 @@ mod test {
     // Detect AES in ECB mode.
     // https://cryptopals.com/sets/1/challenges/8
     assert_eq!(
-      "d880619740a8a19b7840a8a31c810a3d08649af70dc06f4fd5d2d69c744cd283e2dd052f6b641dbf9d11b0348542bb5708649af70dc06f4fd5d2d69c744cd2839475c9dfdbc1d46597949d9c7e82bf5a08649af70dc06f4fd5d2d69c744cd28397a93eab8d6aecd566489154789a6b0308649af70dc06f4fd5d2d69c744cd283d403180c98c8f6db1f2a3f9c4040deb0ab51b29933f2c123c58386b06fba186a",
-      find_aes_ecb("input/08").unwrap());
+      find_aes_ecb("input/08").unwrap(),
+      "d880619740a8a19b7840a8a31c810a3d08649af70dc06f4fd5d2d69c744cd283e2dd052f6b641dbf9d11b0348542bb5708649af70dc06f4fd5d2d69c744cd2839475c9dfdbc1d46597949d9c7e82bf5a08649af70dc06f4fd5d2d69c744cd28397a93eab8d6aecd566489154789a6b0308649af70dc06f4fd5d2d69c744cd283d403180c98c8f6db1f2a3f9c4040deb0ab51b29933f2c123c58386b06fba186a");
   }
 
   #[test]
@@ -140,10 +140,10 @@ mod test {
     let iv = vec![0; 16];
     let res = decrypt_aes_128_cbc(&data, key, &iv).unwrap();
     assert_eq!(
-      "Play that funky music ",
-      String::from_utf8_lossy(&res).lines().last().unwrap()
+      String::from_utf8_lossy(&res).lines().last().unwrap(),
+      "Play that funky music "
     );
-    assert_eq!(2876, res.len());
+    assert_eq!(res.len(), 2876);
   }
 
   #[test]
